2e1112fc31c96b899d19f7f8afd89c7e08cf6dd0
sev-n <nielsevilla39@gmail.com>
2023-05-10 19:30:15 +0800

    refactored

---
+++ lib/model/color_model.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:translate/pages/utils/colors.dart';
+
+class ThemeManager {
+  static bool isDarkMode = false;
+
+  static const IconData _iconLight = Icons.wb_sunny;
+  static const IconData _iconDark = Icons.nights_stay;
+  static Color get navColor => isDarkMode ? vbg : mcgpalette0;
+
+  static final ThemeData _lightTheme =
+      ThemeData(primarySwatch: lightColor, brightness: Brightness.light);
+
+  static final ThemeData _darkTheme =
+      ThemeData(primarySwatch: vbg, brightness: Brightness.dark);
+
+  static get lightTheme => _lightTheme;
+  static get darkTheme => _darkTheme;
+
+  static get iconDark => _iconDark;
+  static get iconLight => _iconLight;
+
+  static Color get appTheme => accent;
+}
