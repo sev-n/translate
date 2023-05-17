57a31631de2d18a77f23b674553f35f849a0fd40
sev-n <nielsevilla39@gmail.com>
2023-05-17 17:19:38 +0800

    refactored

--- lib/pages/bottom_nav_pages/discover pages/Beach/Beaches.dart
+++ lib/pages/bottom_nav_pages/discover pages/Beach/Beaches.dart
@@ -39,60 +39,54 @@ class Beaches extends StatelessWidget {
           SliverToBoxAdapter(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Container(
-                      color: const Color(0xff393E46),
-                      height: 500,
-                      // Add an Image widget here with your desired image source
-                      child: Align(
-                        alignment: Alignment.topCenter,
-                        child: Image.asset(
-                          'assets/Beaches/ELNIDO.jpg',
-                          fit: BoxFit.cover,
-                        ),
+              child: Container(
+                height: 500,
+                decoration: BoxDecoration(
+                    color: const Color(0xff393E46),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: Image.asset(
+                                'assets/Beaches/ELNIDO.jpg',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 40),
+                            child: Text(
+                              'EL Nido',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 40),
+                            child: Text(
+                              "El Nido, which is the well-known region of Palawan, consistently ranks among the world's picturesque beaches, giving it the moniker 'Heaven on Earth'. The beaches and islands of El Nido, look like a hidden adventure since they are surrounded by old limestone cliffs, yet this paradise is actually a well-known getaway.",
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'EL Nido',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                      height:
-                          10), // Added SizedBox to create spacing between title and description
-                  const Text(
-                    "El Nido, which is the well-known region of Palawan, consistently ranks among the world's picturesque beaches, giving it the moniker 'Heaven on Earth'. The beaches and islands of El Nido, look like a hidden adventure since they are surrounded by old limestone cliffs, yet this paradise is actually a well-known getaway.",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  color: const Color(0xff393E46),
-                  height: 400,
+                  ],
                 ),
               ),
             ),
@@ -114,3 +108,51 @@ class Beaches extends StatelessWidget {
     );
   }
 }
+
+
+// Padding(
+//               padding: const EdgeInsets.all(20.0),
+//               child: Column(
+//                 children: [
+//                   ClipRRect(
+//                     borderRadius: BorderRadius.circular(20),
+//                     child: Container(
+//                       color: const Color(0xff393E46),
+//                       height: 500,
+//                       // Add an Image widget here with your desired image source
+//                       child: Align(
+//                         alignment: Alignment.topCenter,
+//                         child: Image.asset(
+//                           'assets/Beaches/ELNIDO.jpg',
+//                           fit: BoxFit.cover,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   const Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Text(
+//                         'EL Nido',
+//                         style: TextStyle(
+//                           color: Colors.white,
+//                           fontSize: 16,
+//                           fontWeight: FontWeight.bold,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                   const SizedBox(
+//                       height:
+//                           10), // Added SizedBox to create spacing between title and description
+//                   const Text(
+//                     "El Nido, which is the well-known region of Palawan, consistently ranks among the world's picturesque beaches, giving it the moniker 'Heaven on Earth'. The beaches and islands of El Nido, look like a hidden adventure since they are surrounded by old limestone cliffs, yet this paradise is actually a well-known getaway.",
+//                     style: TextStyle(
+//                       fontSize: 14,
+//                       color: Colors.white,
+//                       fontWeight: FontWeight.w500,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
