1869138b22d538b51b9b7779e92f901ec3876cd1
Jchael12 <johnmichaelabbas23@gmail.com>
2023-05-21 22:08:46 +0800

    Cultural Contents

--- lib/pages/bottom_nav_pages/discover pages/Cultural/Cultures.dart
+++ lib/pages/bottom_nav_pages/discover pages/Cultural/Cultures.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: file_names
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 import 'package:translate/utils/colors.dart';
 
@@ -11,49 +11,262 @@ class Cultures extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: darkColor,
-      body: Center(
-        child: Card(
-          color: darkColor,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset('assets/Cultural/INTRAMUROS.jpg'),
-              Container(
-                color: darkColor,
-                child: const Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Intramuros',
-                        style: TextStyle(
-                          fontFamily: 'gothic',
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 22,
-                      ),
-                      Text(
-                        "Intramuros is ancient walled city in Manila, it is one of the top spots that tourist visit here in the Philippines. It was build by Spaniards in 1521. It was designed as a tight grid to keep its streets functional but contained. Today, this is the only district in manila that has been successful in keeping its old Spanish Era look,",
-                        style: TextStyle(
-                          fontFamily: 'gothic',
-                          fontSize: 18,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      )
-                    ],
-                  ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: accent,
+            leading: const Icon(Icons.menu),
+            expandedHeight: 250,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                'assets/Cultural/CAC.png',
+                fit: BoxFit.cover,
+              ),
+              titlePadding: EdgeInsets.only(top: 5.h, bottom: 10.h),
+              title: Text(
+                "C U L T U R A L",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'gothic',
+                  fontSize: 22.sp,
                 ),
               ),
-            ],
+              centerTitle: true,
+            ),
           ),
-        ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(20.0.w),
+              child: Container(
+                height: 550.h,
+                decoration: BoxDecoration(
+                  color: const Color(0xff393E46),
+                  borderRadius: BorderRadius.circular(20.w),
+                ),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20.0.w),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.w),
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: Image.asset(
+                                'assets/Cultural/INTRAMUROS.jpg',
+                                fit: BoxFit.cover,
+                                width: 400.w,
+                                height: 250.h,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 40.h),
+                            child: Text(
+                              'Intramuros',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 40.h),
+                            child: Text(
+                              "Intramuros is an ancient walled city in Manila, it is one of the top spots that tourists visit here in the Philippines. It was built by Spaniards in 1521 it was designed as a tight grid to keep its street functional but contained. Today, this is the only district in manila that has been successful in keeping its old Spanish-era look.",
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                fontSize: 14.sp,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(20.0.w),
+              child: Container(
+                height: 500.h,
+                decoration: BoxDecoration(
+                  color: const Color(0xff393E46),
+                  borderRadius: BorderRadius.circular(20.w),
+                ),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20.0.w),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.w),
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: Image.asset(
+                                'assets/Cultural/RIZAL.jpg',
+                                fit: BoxFit.cover,
+                                width: 400.w,
+                                height: 250.h,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 20.h),
+                            child: Text(
+                              'Rizal Park',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 20.h),
+                            child: Text(
+                              "Rizal Park, also known as Luneta Park. It is an iconic and one of the most visited by tourist from other countries or from other provinces. This park carries a rich history as it is the location of Rizal's (Philippine National Hero) execution. The place was also called Bagumbayan during the spanish colonization in the Philippines.",
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                fontSize: 14.sp,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(20.0.w),
+              child: Container(
+                height: 500.h,
+                decoration: BoxDecoration(
+                  color: const Color(0xff393E46),
+                  borderRadius: BorderRadius.circular(20.w),
+                ),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20.0.w),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20.w),
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: Image.asset(
+                                'assets/Cultural/QUIAPO.jpg',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 40.h),
+                            child: Text(
+                              'Quiapo Church',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 40.h),
+                            child: Text(
+                              "Quiapo Church is also known as the minor Basilica of the Black Nazarene or Saint John the Baptist Parish. It is a prominent basilica in the district of Quiapo Manila. The church is famous because it is the home of Black Nazarene, a black statue of Jesus Christ that sait to be miraculous. Quiapo Church holds a novena every friday in honor of the Black Nazarene.",
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                fontSize: 14.sp,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(20.0.w),
+              child: Container(
+                height: 550.h,
+                decoration: BoxDecoration(
+                  color: const Color(0xff393E46),
+                  borderRadius: BorderRadius.circular(20.w),
+                ),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20.0.w),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.w),
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: Image.asset(
+                                'assets/Cultural/NATIONALMUSEUM.jpg',
+                                fit: BoxFit.cover,
+                                width: 400.w,
+                                height: 200.h,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 40.h),
+                            child: Text(
+                              'National Museum of the Philippines',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 20.h),
+                            child: Text(
+                              "National Museum of Fine Arts is a home to 29 galleries and hallway exhibitions of 19th century Filipino masters National Artists, leading modern painters, sculptors, and printmakers. The National Museum of Anthropology stages the Philippine ethnographic and terrestrial and underwater archaeological collection narrating the story of the Philippines from the past. National Museum of History is houses 12 permanent galleries that exhibit the biological and geological diversity of the Philippines.",
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                fontSize: 14.sp,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
