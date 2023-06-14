4f70936ba0a71846f3ffb23793e77e4213f4df39
sev-n <nielsevilla39@gmail.com>
2023-06-14 11:22:19 +0800

    changed ranged value

--- lib/model/swap_lang.dart
+++ lib/model/swap_lang.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class Swap extends ChangeNotifier {
   int touch = 1;
-  
+
   int get touchState => touch;
 
   void setTouchState() {
     touch += 1;
-    if(touch == 10){
+    if(touch == 11){
       touch = 1;
     }
     notifyListeners();
