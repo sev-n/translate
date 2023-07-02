36abc476499089cb5763a8590222544b736d81cf
sev-n <nielsevilla39@gmail.com>
2023-07-02 22:43:52 +0800

    Added default text

--- lib/pages/bottom_nav_pages/UI/history.dart
+++ lib/pages/bottom_nav_pages/UI/history.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:translate/model/stt.dart';
 import 'package:translate/utils/colors.dart';
@@ -14,28 +15,39 @@ class History extends StatelessWidget {
       color: darkColor,
       child: Consumer<LanguagesSpokeStt>(
         builder: (context, langStt, child) {
-          return ListView.builder(
-            itemCount: langStt.history.length,
-            itemBuilder: (context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Dismissible(
-                  key: UniqueKey(),
-                  onDismissed: (DismissDirection direction){
-                    langStt.removeItemHistory(index);
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xff393E46),
+          return langStt.history.isEmpty
+              ? Center(
+                  child: Text(
+                    "History is empty",
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      fontFamily: 'gothic',
+                      color: const Color(0xffEEEEEE),
                     ),
-                    child: langStt.history[index],
                   ),
-                ),
-              );
-            },
-          );
+                )
+              : ListView.builder(
+                  itemCount: langStt.history.length,
+                  itemBuilder: (context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Dismissible(
+                        key: UniqueKey(),
+                        onDismissed: (DismissDirection direction) {
+                          langStt.removeItemHistory(index);
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xff393E46),
+                          ),
+                          child: langStt.history[index],
+                        ),
+                      ),
+                    );
+                  },
+                );
         },
       ),
     );
