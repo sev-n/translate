94b6cc11adc4420fca3eeed97e81e1614394b585
sev-n <nielsevilla39@gmail.com>
2023-07-05 20:10:55 +0800

    refactored

--- lib/pages/bottom_nav_pages/discover pages/China/china.dart
+++ lib/pages/bottom_nav_pages/discover pages/China/china.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:translate/utils/colors.dart';
 
+import '../custom_click.dart';
+
 class China extends StatelessWidget {
   const China({super.key});
 
@@ -179,27 +181,6 @@ class China extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class Clickable extends StatelessWidget {
-  final Widget child;
-  final String destination;
-  const Clickable({super.key, required this.child, required this.destination});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      type: MaterialType.transparency,
-      child: InkWell(
-        onTap: () {
-          Navigator.pushNamed(context, destination);
-        },
-        borderRadius: BorderRadius.circular(10.0.r),
-        splashColor: Colors.black.withOpacity(0.3),
-        child: child,
       ),
     );
   }
--- lib/pages/bottom_nav_pages/discover pages/Korea/korea.dart
+++ lib/pages/bottom_nav_pages/discover pages/Korea/korea.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:translate/utils/colors.dart';
 
+import '../custom_click.dart';
+
 class Korea extends StatelessWidget {
   const Korea({super.key});
 
@@ -180,26 +182,5 @@ class Korea extends StatelessWidget {
             ),
           ),
         ));
-  }
-}
-
-class Clickable extends StatelessWidget {
-  final Widget child;
-  final String destination;
-  const Clickable({super.key, required this.child, required this.destination});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      type: MaterialType.transparency,
-      child: InkWell(
-        onTap: () {
-          Navigator.pushNamed(context, destination);
-        },
-        borderRadius: BorderRadius.circular(10.0.r),
-        splashColor: Colors.black.withOpacity(0.3),
-        child: child,
-      ),
-    );
   }
 }
